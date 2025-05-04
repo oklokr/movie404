c678072fd2fe6bf704b5dafcff8df7cd36aca298
lacieliz <lacieliz@naver.com>
2025-05-04 21:37:16 +0900

    250504

--- front/src/page/auth/login/index.jsx
+++ front/src/page/auth/login/index.jsx
@@ -7,7 +7,12 @@ function login() {
     console.log(res)
   })
 
-  return <div>login</div>
+  return(
+    <>
+  <div>로그인로그인페이지 여기다가 코딩코딩 ㄱㄱ</div>
+  <table><tr><th>테이블임</th><td>테이브으을</td></tr><tr><td>테이블임임</td><td>테이브으으을</td></tr></table>
+  </>);
+  
 }
 
 export default login
