845a76896b25edc275b644d43415398451e939d9
oklokr <aig741@naver.com>
2025-05-07 18:40:46 +0900

    router경로 모든파일 호출 수정중

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,20 +1,15 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { RouterProvider } from "react-router"
-import router from "./routes"
-import Header from "@/layout/header"
-import Footer from "@/layout/footer"
 import "@/locales/i18n"
 
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
+import Router from "@/routes"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
-    <RouterProvider router={router} />
-    <Footer />
+    <Router />
   </StrictMode>,
 )
--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,9 +1,33 @@
-import { createBrowserRouter, RouterProvider } from "react-router"
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+  useLocation,
+} from "react-router"
 import Page404 from "@/page/404"
+import Header from "@/layout/header"
+import Footer from "@/layout/footer"
+import admin from "@/routes/pages/admin"
+import auth from "@/routes/pages/auth"
+import community from "@/routes/pages/community"
+import main from "@/routes/pages/main"
+import { useEffect, useState } from "react"
 
-const modules = import.meta.glob("./pages/*.jsx", { eager: true })
-const routers = Object.values(modules).flatMap((module) => module.default)
-const router = createBrowserRouter([
+// const modules = import.meta.glob("./pages/*.jsx")
+// const pageRoutes = Object.values(modules).flatMap((m) => m.default)
+
+function requiresAuth(res) {
+  let state = true
+  // if (state) {
+  //   console.log("토큰 없는 경우 리다이렉트")
+  //   return redirect("/login")
+  // } else {
+  //   return redirect("/main")
+  // }
+}
+
+const routes = [
+  // ...pageRoutes,
   {
     path: "/404",
     Component: Page404,
@@ -11,8 +35,21 @@ const router = createBrowserRouter([
   {
     path: "*",
     Component: Page404,
+    // loader: requiresAuth,
   },
-  ...routers,
-])
+]
 
-export default router
+// const router = createBrowserRouter(routes)
+export default function Router() {
+  // const [isHerf, setHerf] = useState("")
+  // setHerf(window.location.pathname)
+  // console.log(isHerf)
+  return (
+    <>
+      <div>test</div>
+      {/* <Header /> */}
+      {/* <RouterProvider router={router} /> */}
+      {/* <Footer /> */}
+    </>
+  )
+}
