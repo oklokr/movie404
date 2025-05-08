abf86020dfae3cfd6bac342534ea106cb552acab
oklokr <aig741@naver.com>
2025-05-08 15:43:42 +0900

    vite ts 오류 수정

--- front/src/page/auth/login/index.jsx
+++ front/src/page/auth/login/index.jsx
@@ -1,13 +1,73 @@
 import { mypageLoginInfo } from "@/api/sample"
+import { Visibility, VisibilityOff } from "@mui/icons-material"
+import { css } from "@emotion/react"
+import {
+  Box,
+  FilledInput,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material"
+import { useState } from "react"
 
-function login() {
-  mypageLoginInfo({
-    text: "false",
-  }).then((res) => {
-    console.log(res)
-  })
+const style = css`
+  color: red;
+`
 
-  return <div>login</div>
+function Login() {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+  // mypageLoginInfo({
+  //   text: "false",
+  // }).then((res) => {
+  //   console.log(res)
+  // })
+
+  return (
+    <div>
+      <h1
+        css={css`
+          color: teal;
+          font-size: 24px;
+        `}
+      >
+        로고
+      </h1>
+      <h2>로그인</h2>
+      <div className="input-wrap">
+        <InputLabel htmlFor="id">아이디</InputLabel>
+        <OutlinedInput id="id" label="id" />
+      </div>
+      <div>
+        <InputLabel htmlFor="password">패스워드</InputLabel>
+        <OutlinedInput
+          id="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={
+                  showPassword ? "hide the password" : "display the password"
+                }
+                onClick={handleClickShowPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </div>
+    </div>
+  )
 }
 
-export default login
+export default Login
