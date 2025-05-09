51b6691ef382a863238986bc9431e25e7f294578
oklokr <aig741@naver.com>
2025-05-09 14:18:01 +0900

    공통코드 추가 및 login 작성중

--- front/src/page/auth/login/index.jsx
+++ front/src/page/auth/login/index.jsx
@@ -1,5 +1,5 @@
 import { mypageLoginInfo } from "@/api/sample"
-import { Visibility, VisibilityOff } from "@mui/icons-material"
+import { Password, Visibility, VisibilityOff } from "@mui/icons-material"
 import { css } from "@emotion/react"
 import {
   Button,
@@ -8,7 +8,7 @@ import {
   InputLabel,
   OutlinedInput,
 } from "@mui/material"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import logoImg from "@/assets/images/logo/logo.png"
 
 const loginWrapStyle = css`
@@ -38,7 +38,12 @@ const loginBoxStyle = css`
 `
 
 function Login() {
+  const defaultPostForm = {
+    id: "",
+    password: "",
+  }
   const [showPassword, setShowPassword] = useState(false)
+  const [postForm, setPostForm] = useState(defaultPostForm)
   const handleClickShowPassword = () => setShowPassword((show) => !show)
   const handleLogin = () => {
     mypageLoginInfo({
@@ -47,6 +52,10 @@ function Login() {
       console.log(res)
     })
   }
+
+  useEffect(() => {
+    console.log("폼 변경됨:", postForm)
+  }, [postForm])
 
   return (
     <div css={loginWrapStyle}>
@@ -57,7 +66,12 @@ function Login() {
         <h2>로그인</h2>
         <div className="input-form input-form--row">
           <InputLabel htmlFor="id">아이디</InputLabel>
-          <OutlinedInput id="id" label="id" />
+          <OutlinedInput
+            id="id"
+            label="id"
+            value={postForm.id}
+            onChange={(e) => setPostForm({ ...postForm, id: e.target.value })}
+          />
         </div>
         <div>
           <InputLabel htmlFor="password">패스워드</InputLabel>
@@ -65,6 +79,10 @@ function Login() {
             id="password"
             label="Password"
             type={showPassword ? "text" : "password"}
+            value={postForm.password}
+            onChange={(e) =>
+              setPostForm({ ...postForm, password: e.target.value })
+            }
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
