b027ac1bb1cc4319377936419dc8622f047c77c6
lacieliz <lacieliz@naver.com>
2025-05-09 14:57:49 +0900

    회원가입 이메일 작업중중

--- front/src/page/auth/signup/index.jsx
+++ front/src/page/auth/signup/index.jsx
@@ -9,33 +9,62 @@ import { Visibility, VisibilityOff } from "@mui/icons-material"
 import InputLabel from "@mui/material/InputLabel"
 import InputAdornment from "@mui/material/InputAdornment"
 import IconButton from "@mui/material/IconButton"
+import { FormHelperText } from "@mui/material"
 
-const test = {
-  color: "black",
-}
 function signup() {
-  const [test2, setTest2] = useState("아이디를 입력해주세요")
-  const [test3, setTest3] = useState("비밀번호를 입력해주세요")
+  const [commentId, setCommentId] = useState("아이디를 입력해주세요")
+  const [commentPW, setCommentPW] = useState("비밀번호를 입력해주세요")
+  const [commentRPW, setCommentRPW] = useState("비밀번호 재확인을 입력해주세요")
+  const [commentEmail, setCommentEmail] = useState("s")
   const [showPassword, setShowPassword] = useState(false)
+
+  const [PW, setPW] = useState("")
+
   const handleClickShowPassword = () => setShowPassword((show) => !show)
 
-  function handleChange(e) {
+  function handleChangeID(e) {
     const val = e.target.value
-    if (/[!@#$%^&*|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(val)) setTest2("특수문자/한글 포함 불가")
-    else setTest2("아이디를 입력해주세요")
+    if (val === "") setCommentId("아이디를 입력해주세요")
+    if (/[!@#$%^&*|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(val)) setCommentId("특수문자/한글 포함 불가")
+    else setCommentId("아이디를 입력 완료!")
   }
-
+  function handleChangePW(e) {
+    const val = e.target.value
+    if (val === "") setCommentPW("비밀번호를 입력해주세요")
+    else if (/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(val)) setCommentPW("한글 포함 불가")
+    else if (!/[!@#$%^&*]/.test(val)) setCommentPW("특수문자를 포함해주세요")
+    else setCommentPW("비밀번호 입력 완료!")
+    //else setC
+  }
+  function handleChangeRPW(e) {
+    const val = e.target.value
+    if (val === "") setCommentRPW("비밀번호 재확인을 입력해주세요")
+    else if (/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(val)) setCommentRPW("한글 포함 불가")
+    else if (!/[!@#$%^&*]/.test(val)) setCommentRPW("특수문자를 포함해주세요")
+    //else if
+    else setCommentRPW("비밀번호 입력 완료!")
+    //else setC
+  }
+  function handleChangeRPW(e) {
+    const val = e.target.value
+    if (val === "") setCommentRPW("비밀번호 재확인을 입력해주세요")
+    else if (/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(val)) setCommentRPW("한글 포함 불가")
+    else if (!/[!@#$%^&*]/.test(val)) setCommentRPW("특수문자를 포함해주세요")
+    //else if
+    else setCommentRPW("비밀번호 입력 완료!")
+    //else setC
+  }
   return (
     <>
-      <h1 style={test}> 정보 입력 </h1>
+      <h1> 정보 입력 </h1>
       <div>
         <InputLabel className="input-form">아이디 </InputLabel>
         <TextField
           id="signup_id"
           aria-describedby="outlined-weight-helper-text"
           required
-          helperText={test2}
-          onChange={handleChange}
+          helperText={commentId}
+          onChange={handleChangeID}
         />
         <Button variant="contained">중복확인</Button>
       </div>
@@ -46,6 +75,9 @@ function signup() {
           required
           id="signup_pwd"
           type={showPassword ? "text" : "password"}
+          onChange={() => {
+            handleChangePW
+          }}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -56,19 +88,51 @@ function signup() {
               </IconButton>
             </InputAdornment>
           }
-
           //helperText={}
           //onChange={}
         />
+        <FormHelperText>{commentPW}</FormHelperText>
       </div>
 
       <div>
-        <InputLabel>비밀번호 확인 </InputLabel>
-        <OutlinedInput id="signup_repwd" aria-describedby="outlined-weight-helper-text" required />
+        <InputLabel>비밀번호 재확인 </InputLabel>
+        <OutlinedInput
+          aria-describedby="outlined-weight-helper-text"
+          required
+          id="signup_pwd"
+          type={showPassword ? "text" : "password"}
+          onChange={handleChangeRPW}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={showPassword ? "hide the password" : "display the password"}
+                onClick={handleClickShowPassword}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          //helperText={}
+          //onChange={}
+        />
+        <FormHelperText>{commentRPW}</FormHelperText>
       </div>
+
       <div>
         <InputLabel>이메일 </InputLabel>
-        <OutlinedInput id="signup_email" aria-describedby="outlined-weight-helper-text" required />
+        <OutlinedInput
+          id="signup_email"
+          aria-describedby="outlined-weight-helper-text"
+          required
+          // onChange={handleChangeEmail}
+        />
+        <></>
+        <FormHelperText>{commentEmail}</FormHelperText>
+        <select>
+          <option value="google">구글</option>
+          <option value="naver">네이버</option>
+          <option value="daum">다음</option>
+        </select>
       </div>
       <div>
         <InputLabel>전화번호 </InputLabel>
