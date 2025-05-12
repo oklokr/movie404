52f649d905b97b575e5ea0a118cf97abc3b432bb
lh <twin991015@gmail.com>
2025-05-12 12:25:08 +0900

    [2025.05.12 변경사항] 관리자 유저 조회 구현 완료 검색기능 동작( 추후 관리자만 접근 가능하게 막아야됨 - 로그인 구현 되면)

---
+++ backend/src/main/java/com/project/controller/admin/AdminController.java
@@ -0,0 +1,22 @@
+package com.project.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.model.UserDto;
+import com.project.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user")
+    public List<UserDto> getUserList() {
+        return userService.getUserList();
+    }
+}
