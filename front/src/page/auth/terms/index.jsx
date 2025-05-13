7fede2e8859d1ae0818d8b491476fe02923627cc
lacieliz <lacieliz@naver.com>
2025-05-13 12:46:55 +0900

    약관 작업중

--- front/src/page/auth/terms/index.jsx
+++ front/src/page/auth/terms/index.jsx
@@ -1,9 +1,14 @@
-import * as React from "react"
 import Accordion from "@mui/material/Accordion"
 import AccordionSummary from "@mui/material/AccordionSummary"
 import AccordionDetails from "@mui/material/AccordionDetails"
 import Typography from "@mui/material/Typography"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import Radio from "@mui/material/Radio"
+import RadioGroup from "@mui/material/RadioGroup"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import FormControl from "@mui/material/FormControl"
+const cssAlign = { "justify-content": "right" }
+const cssWidth = { width: "100%" }
 
 function terms() {
   return (
@@ -25,6 +30,18 @@ function terms() {
             </Typography>
           </AccordionDetails>
         </Accordion>
+
+        <FormControl css={cssWidth}>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            css={cssAlign}
+          >
+            <FormControlLabel value="agree" control={<Radio />} label="동의하지 않음" />
+            <FormControlLabel value="disagree" control={<Radio />} label="동의함" />
+          </RadioGroup>
+        </FormControl>
         <Accordion>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
@@ -40,6 +57,69 @@ function terms() {
             </Typography>
           </AccordionDetails>
         </Accordion>
+        <FormControl css={cssWidth}>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            css={cssAlign}
+          >
+            <FormControlLabel value="agree" control={<Radio />} label="동의하지 않음" />
+            <FormControlLabel value="disagree" control={<Radio />} label="동의함" />
+          </RadioGroup>
+        </FormControl>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">Header</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus
+              ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <FormControl css={cssWidth}>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            css={cssAlign}
+          >
+            <FormControlLabel value="agree" control={<Radio />} label="동의하지 않음" />
+            <FormControlLabel value="disagree" control={<Radio />} label="동의함" />
+          </RadioGroup>
+        </FormControl>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">Header</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus
+              ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <FormControl css={cssWidth}>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            css={cssAlign}
+          >
+            <FormControlLabel value="agree" control={<Radio />} label="동의하지 않음" />
+            <FormControlLabel value="disagree" control={<Radio />} label="동의함" />
+          </RadioGroup>
+        </FormControl>
       </div>
     </>
   )
