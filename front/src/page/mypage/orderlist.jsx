933b7d28aed81cf2fa22312e89e1471e143b453f
lacieliz <lacieliz@naver.com>
2025-05-14 14:49:51 +0900

    mypage

--- front/src/page/mypage/orderlist.jsx
+++ front/src/page/mypage/orderlist.jsx
@@ -9,26 +9,27 @@ import {
 } from "@mui/material"
 import { useEffect, useState } from "react"
 var btn = 1
-useEffect(() => {}, [btn])
+//useEffect(() => {}, [btn])
+function menuClick(e) {
+  alert(e.target.id)
+  btn = 2
+  return btn
+}
 function OrderListMenu() {
   return (
     <>
       <Button
-        id="basic-button"
+        id="orderlist"
         href="#basic#user"
         size="large"
         css={Leftbtn}
-        onClick={() => (btn = 1)}
+        onClick={(e) => {
+          return 0
+        }}
       >
         결제내역
       </Button>
-      <Button
-        id="basic-button"
-        href="#basic#user"
-        size="large"
-        css={Leftbtn}
-        onClick={() => (btn = 2)}
-      >
+      <Button id="payment" href="#basic#user" size="large" css={Leftbtn} onClick={menuClick}>
         카드관리
       </Button>
     </>
--- front/src/page/mypage/usersetting.jsx
+++ front/src/page/mypage/usersetting.jsx
@@ -1,10 +1,14 @@
 import {
   Button,
+  FormControl,
+  FormControlLabel,
   FormHelperText,
   IconButton,
   InputAdornment,
   InputLabel,
   OutlinedInput,
+  Radio,
+  RadioGroup,
   TextField,
 } from "@mui/material"
 
@@ -16,30 +20,105 @@ function UserSetMenu() {
   )
 }
 function UserSet() {
-  const mailformat = [
+  const langformat = [
     {
-      value: "naver",
-      label: "@naver.com",
+      value: "kor",
+      label: "한국어",
     },
     {
-      value: "google",
-      label: "@gmail.com",
+      value: "eng",
+      label: "English",
+    },
+  ]
+
+  const dateformat = [
+    {
+      value: "YMD",
+      label: "YYYY-MM-DD",
     },
     {
-      value: "daum",
-      label: "@daum.com",
-    },
-    {
-      value: "nate",
-      label: "@nate.com",
+      value: "DMY",
+      label: "DD-MM-YYYY",
     },
   ]
   return (
     <>
       <h1>기본설정</h1>
+
+      <div className="input-form">
+        <InputLabel>언어설정 </InputLabel>
+        <div>
+          <TextField
+            id="outlined-select-currency-native"
+            //onChange={handleSelectEmail}
+            select
+            slotProps={{
+              select: {
+                native: true,
+              },
+            }}
+          >
+            {langformat.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+        </div>
+      </div>
+      <div className="input-form">
+        <InputLabel>날짜설정 </InputLabel>
+        <div>
+          <TextField
+            id="outlined-select-currency-native"
+            //onChange={handleSelectEmail}
+            select
+            slotProps={{
+              select: {
+                native: true,
+              },
+            }}
+          >
+            {dateformat.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+        </div>
+      </div>
+      <div className="input-form">
+        <InputLabel>컨텐츠 </InputLabel>
+
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+          >
+            <FormControlLabel value="0" control={<Radio />} label="성인 컨텐츠 보기" />
+            <FormControlLabel value="1" control={<Radio />} label="검색 기록 저장" />
+          </RadioGroup>
+        </FormControl>
+      </div>
+      <div className="input-form">
+        <InputLabel>성인인증 </InputLabel>
+        <TextField
+          id="signup_id"
+          aria-describedby="outlined-weight-helper-text"
+          required
+          //helperText={commentId}
+          //onChange={handleChangeID}
+        />
+        <Button variant="contained">인증요청</Button>
+      </div>
+
+      <Button variant="contained">저장</Button>
     </>
   )
 }
+const cssWidth = { width: "100%" }
+
 const Leftbtn = {
   "font-size": "1.3rem",
   align: "center",
