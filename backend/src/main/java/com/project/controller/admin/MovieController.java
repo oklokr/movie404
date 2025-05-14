4532976942d3b004256ed3847dffde0944cba4f9
lh <twin991015@gmail.com>
2025-05-14 15:15:09 +0900

    [2025.05.14 수정사항] 영화 등록까진 됐는데 영화 목록에서 사진 안뜨는거랑 상세에서 정보 못불러오는거 수정해야됨

--- backend/src/main/java/com/project/controller/admin/MovieController.java
+++ backend/src/main/java/com/project/controller/admin/MovieController.java
@@ -3,6 +3,7 @@ package com.project.controller.admin;
 import com.project.model.MovieDto;
 import com.project.service.MovieService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -38,5 +39,18 @@ public class MovieController {
     @DeleteMapping("/{movieCode}")
     public void deleteMovie(@PathVariable("movieCode") String movieCode) {
         movieService.deleteMovie(movieCode);
+    }
+
+    @PostMapping
+    public void createMovie(
+            @RequestParam Map<String, String> allParams,
+            @RequestParam(value = "POSTER", required = false) MultipartFile poster
+    ) {
+        movieService.createMovie(allParams, poster);
+    }
+
+    @GetMapping("/creator")
+    public List<Map<String, Object>> getCreatorList() {
+        return movieService.getCreatorList();
     }
 }
--- backend/src/main/java/com/project/model/MovieDto.java
+++ backend/src/main/java/com/project/model/MovieDto.java
@@ -7,10 +7,22 @@ import lombok.Setter;
 @Setter
 public class MovieDto {
     private String movieCode;
+    private String genreCodeA;
+    private String genreCodeB;
+    private String genreCodeC;
     private String movieName;
+    private String directCodeA;
+    private String directCodeB;
+    private String actorCodeA;
+    private String actorCodeB;
+    private String actorCodeC;
+    private String actorCodeD;
+    private String actorCodeE;
+    private String synopsis;
+    private String runtime;      // "HH:mm:ss" 형식 문자열로 받음
+    private String ratingTpcd;
+    private String movieRelease; // "yyyy-MM-dd"
+    private String teaser;
     private String poster;
-    private Integer dvdPrice;
-    private Integer dvdDiscount;
-    private Integer reservePrice;
-    private Integer reserveDiscount;
+    private Long sales;
 }
