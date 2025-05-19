2d2ec1fc54e3eff89f946be00f1dcedb37222624
oklokr <aig741@naver.com>
2025-05-19 23:54:54 +0900

    메인 페이지 작업중중

--- front/src/page/main/index.jsx
+++ front/src/page/main/index.jsx
@@ -1,15 +1,84 @@
 import { mainGetMovieList } from "@/api/main"
+import { css } from "@emotion/react"
+import { useEffect, useState } from "react"
+import { Navigation } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/react"
 
 function main() {
-  mainGetMovieList({
-    schedule: "1",
-  }).then((res) => {
-    const { code, data } = res
-    if (code !== 200) return console.log("test")
-    console.log(data)
+  const [fetchData, setFetchData] = useState({
+    showMovies: [],
+    allMovie: [],
+    randomMovie: [],
   })
 
-  return <div>main</div>
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const showMovie = await mainGetMovieList({ schedule: "1" })
+      const allMovie = await mainGetMovieList({ size: 28 })
+
+      // 랜덤 5개 추출
+      const randomIndexes = []
+      while (randomIndexes.length < 5) {
+        const randomIndex = Math.floor(Math.random() * allMovie.data.list.length)
+        if (!randomIndexes.includes(randomIndex)) {
+          randomIndexes.push(randomIndex)
+        }
+      }
+
+      // 랜덤으로 추출한 값 저장
+      const randomMovie = randomIndexes.map((index) => allMovie.data.list[index])
+
+      // 랜덤으로 추출한 값을 제외한 나머지 값 저장
+      const filteredAllMovie = allMovie.data.list.filter(
+        (_, index) => !randomIndexes.includes(index),
+      )
+
+      // 상태 업데이트
+      setFetchData({
+        showMovies: showMovie.data.list,
+        allMovie: filteredAllMovie,
+        randomMovie: randomMovie,
+      })
+    }
+
+    fetchMovie()
+  }, [])
+
+  console.log(fetchData)
+
+  return (
+    <div css={mainStyle}>
+      <section>
+        <h3></h3>
+        <Swiper navigation={true} modules={[Navigation]} className="banner">
+          {fetchData.randomMovie.map((item) => (
+            <SwiperSlide key={item.movieCode}>test</SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+
+      <section>
+        <Swiper navigation={true} modules={[Navigation]} className="banner">
+          {fetchData.randomMovie.map((item) => (
+            <SwiperSlide key={item.movieCode}>test</SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+
+      <section>
+        <Swiper navigation={true} modules={[Navigation]} className="banner">
+          {fetchData.randomMovie.map((item) => (
+            <SwiperSlide key={item.movieCode}>test</SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </div>
+  )
 }
+
+const mainStyle = css`
+  padding-top: 60px;
+  position: relative;
+`
 
 export default main
