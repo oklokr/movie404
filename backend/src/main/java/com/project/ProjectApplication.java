e375a9f58b94ea9245028373230064072bb4f62e
oklokr <aig741@naver.com>
2025-05-22 20:15:21 +0900

    빌드용

--- backend/src/main/java/com/project/ProjectApplication.java
+++ backend/src/main/java/com/project/ProjectApplication.java
@@ -3,16 +3,23 @@ package com.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.project.repository")
-public class ProjectApplication {
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().filename("setting.env").directory("./").load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+public class ProjectApplication extends SpringBootServletInitializer {
 
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().filename("setting.env").directory("./").load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectApplication.class);
+    }
 }
